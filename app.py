621ab3912f207f4b94cd04292b6f351995e09d1b
Olivier G <zotri@hotmail.com>
2024-03-24 15:28:07 +0100

    port tab in a external file

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 import rtmidi
 import tkinter as tk
+import tkinter.ttk as ttk
 import src.menus
+import src.midi_port
 
 
 class TkWindow(tk.Tk): # inherits from tk.Tk
-    def __init__(self, midi_ports):
+    def __init__(self):
         # Call parent class constructor to have its behavior & state under self
         tk.Tk.__init__(self)
-        self.midi_ports = midi_ports
+        self.midi = rtmidi.MidiIn()
+        self.midi_ports = self.midi.get_ports()
         self.geometry("640x480")
         self.title("MIDI Tweaker")
 
         # LAYOUT ##########
         # Application menu
         self.menu_bar = src.menus.MenuBar(self)
-        # MIDI IN bar
-        self.midi_in = tk.Frame(self, height=50, bg='lightblue')
-        self.midi_in.pack(fill='x')
+        self.config(menu=self.menu_bar)
         # Main window
-        self.main = tk.Frame(self)
+        self.main = ttk.Notebook(self)
         self.main.pack(fill='both', expand=True)
-        # MIDI OUT bar
-        self.midi_out = tk.Frame(self, height=50, bg='lightblue')
-        self.midi_out.pack(fill='x')
-
         # Generic message
-        label = tk.Label(self.main, text="Choose a MIDI port")
-        label.pack(fill='both', expand=True)
+        # label = tk.Label(self.main, text="Choose a MIDI port")
+        # label.pack(fill='both', expand=True)
+        self.mainloop()
+
+    def open_port(self, port_name, port_index):
+        # create tab
+        tab = src.midi_port.PortTab(self.main, self.midi, port_name, port_index)
+        self.main.add(tab, text=port_name)
 
 
 # Entry point
 if __name__ == "__main__":
-    midi = rtmidi.MidiIn()
-    midi_ports = midi.get_ports()
-    window = TkWindow(midi_ports)
-    window.mainloop()
+    window = TkWindow()
