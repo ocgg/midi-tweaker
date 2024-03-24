1916924646daeab3e99dab5310cabda15ab77e98
Olivier G <zotri@hotmail.com>
2024-03-24 16:47:58 +0100

    tabs OK

--- src/midi_port.py
+++ src/midi_port.py
@@ -1,9 +1,11 @@
+import rtmidi
 import tkinter as tk
 import tkinter.ttk as ttk
 
 class PortTab(ttk.Frame):
-    def __init__(self, master, midi, port_name, port_index):
+    def __init__(self, master, port_name, port_index):
         super().__init__(master)
+        midi = rtmidi.MidiIn()
         self.midi_in = midi.open_port(port_index)
         self.pack(fill='both', expand=True)
 
@@ -11,7 +13,8 @@ class PortTab(ttk.Frame):
         self.midi_in_bar = tk.Frame(self, height=50, bg='lightblue')
         self.midi_in_bar.pack(fill='x')
         self.midi_in_msg = ttk.Label(self.midi_in_bar, text="MIDI IN")
-        self.midi_in_msg.pack(fill='both', expand=1)
+        # self.midi_in_msg.pack(padx=0.5, pady=0.5, anchor='center')
+        self.midi_in_msg.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         self.midi_in.set_callback(self.midi_in_callback)
 
