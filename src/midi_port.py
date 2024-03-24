86cd708d306ec9813bbcda0b2e5796297e17fb2e
Olivier G <zotri@hotmail.com>
2024-03-24 17:34:55 +0100

    callback for midi_in

--- src/midi_port.py
+++ src/midi_port.py
@@ -7,24 +7,51 @@ class PortTab(ttk.Frame):
         super().__init__(master)
         midi = rtmidi.MidiIn()
         self.midi_in = midi.open_port(port_index)
-        self.pack(fill='both', expand=True)
-
-        # MIDI IN bar
-        self.midi_in_bar = tk.Frame(self, height=50, bg='lightblue')
-        self.midi_in_bar.pack(fill='x')
-        self.midi_in_msg = ttk.Label(self.midi_in_bar, text="MIDI IN")
-        # self.midi_in_msg.pack(padx=0.5, pady=0.5, anchor='center')
-        self.midi_in_msg.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
         self.midi_in.set_callback(self.midi_in_callback)
 
         # Main frame
         self.main = tk.Frame(self)
-        self.main.pack(fill='both', expand=True)
+
+        # MIDI IN bar
+        self.midi_in_bar = tk.Frame(self)
+        self.midi_in_label = ttk.Label(
+            self.midi_in_bar,
+            text="MIDI IN: waiting for messages...",
+            anchor='w',
+            width=50
+        )
+        self.midi_in_label.pack(fill='x')
+        midi_in_separator = ttk.Separator(self.midi_in_bar, orient='horizontal')
+        midi_in_separator.pack(fill='x')
 
         # MIDI OUT bar
         self.midi_out_bar = tk.Frame(self, height=50, bg='lightblue')
+        self.midi_out_label = ttk.Label(
+            self.midi_out_bar,
+            text="MIDI OUT: waiting for messages...",
+            anchor='w',
+            width=50
+        )
+        midi_out_separator = ttk.Separator(self.midi_out_bar, orient='horizontal')
+        midi_out_separator.pack(fill='x')
+        self.midi_out_label.pack(fill='x')
+
+        # PACKING (layout) ##########
+        self.midi_in_bar.pack(fill='x')
+        self.main.pack(fill='both', expand=True)
         self.midi_out_bar.pack(fill='x')
+        self.pack(fill='both', expand=True)
 
     def midi_in_callback(self, msg, data=None):
-        self.midi_in_msg.configure(text=msg)
+        prefix = "MIDI IN: "
+        msg_data = msg[0]
+        channel = msg_data[0] & 0xF
+        msg_type = msg_data[0] & 0xF0
+
+        if msg_type == 0x90:  # Note on
+            txt = f"CH: {channel}   | NOTEON  {msg_data[1]}     | VEL: {msg_data[2]}"
+        elif msg_type == 0x80:    # Note off
+            txt = f"CH: {channel}   | NOTEOFF {msg_data[1]}     | VEL: {msg_data[2]}"
+        elif msg_type == 0xB0:  # CC
+            txt = f"CH: {channel}   | CC    {msg_data[1]}       | VAL: {msg_data[2]}"
+        self.midi_in_label.configure(text=prefix + txt)
