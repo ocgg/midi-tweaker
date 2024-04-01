5c10c33a4c3e5b48388d986d95448c18d550aa4c
Olivier G <zotri@hotmail.com>
2024-04-01 19:06:29 +0200

    ready to add rules

--- src/router/router.py
+++ src/router/router.py
@@ -2,27 +2,32 @@ import mido
 from tkinter import messagebox
 
 
-# Router is what links the GUI with the midi port & rules.
-# A tab has one router,
-# which has one midi in port, many rules & one midi out port
+# Router is the rules controller for one Tab.
+# And it is what links the GUI with the midi port.
 class Router:
     def __init__(self, tab, port_name):
         self.TAB = tab
         self.midi_in = self._open_midi_in(port_name)
-        self.midi_in.callback = self.midi_in_callback
+        self.midi_in.callback = self._midi_in_callback
+        self.midi_out = mido.open_output('gnah', virtual=True)
         self.rules = []
 
     def add_rule(self, rule):
         pass
 
-    def midi_in_callback(self, msg, data=None):
-        prefix = "MIDI IN:\n"
-        self.TAB.midi_in_label.configure(text=prefix + str(msg))
-
     # PRIVATE METHODS #########################################################
+
+    def _midi_in_callback(self, msg, data=None):
+        # Display the message in the GUI
+        self.TAB.midi_in_label.configure(text=f"MIDI_IN: {str(msg)}")
+        # Apply the rules
+        # Send the message to midi out
+        self.midi_out.send(msg)
+        # Display the message in the GUI
+        self.TAB.midi_out_label.configure(text=f"MIDI_OUT: {str(msg)}")
 
     def _open_midi_in(self, port_name):
         try:
             return mido.open_input(port_name)
-        except StopIteration:
+        except IOError:
             messagebox.showerror("Error", "MIDI port not found")
