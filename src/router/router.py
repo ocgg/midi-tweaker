c74050cb498df8f000024c36f29aab7b13e27bb4
Olivier G <zotri@hotmail.com>
2024-04-01 18:37:27 +0200

    rtmidi replaced by mido

--- src/router/router.py
+++ src/router/router.py
@@ -1,7 +1,5 @@
-import rtmidi
-import rtmidi.midiconstants as consts
+import mido
 from tkinter import messagebox
-# from src.router.midi_msg import MidiMsg
 
 
 # Router is what links the GUI with the midi port & rules.
@@ -11,40 +9,20 @@ class Router:
     def __init__(self, tab, port_name):
         self.TAB = tab
         self.midi_in = self._open_midi_in(port_name)
-        self.midi_in.set_callback(self.midi_in_callback)
+        self.midi_in.callback = self.midi_in_callback
         self.rules = []
 
     def add_rule(self, rule):
         pass
 
     def midi_in_callback(self, msg, data=None):
-        # Convert msg to MidiMsg object
-        # midi_msg = MidiMsg(msg)
-        # 1.display midi in message (tab)
-        # 2. apply rules & send to midi out
         prefix = "MIDI IN:\n"
-        msg_data = msg[0]
-        channel = msg_data[0] & 0xF     # lower 4 bits
-        msg_type = msg_data[0] & 0xF0   # upper 4 bits
-
-        if msg_type == consts.NOTE_ON:  # Note on
-            txt = f"NOTEON CH:{channel} NOTE:{msg_data[1]} VEL:{msg_data[2]}"
-        elif msg_type == consts.NOTE_OFF:   # Note off
-            txt = f"NOTEOFF CH:{channel} NOTE:{msg_data[1]} VEL:{msg_data[2]}"
-        elif msg_type == consts.CONTROL_CHANGE:  # CC
-            txt = f"CONTROL CHANGE CH:{channel} CONTROLLER:{msg_data[1]} VAL:{msg_data[2]}"
-            # TODO: handle pitch bend & other General Midi specific messages
-        else:
-            txt = f"Not covered yet: {msg}"
-        self.TAB.midi_in_label.configure(text=prefix + txt)
+        self.TAB.midi_in_label.configure(text=prefix + str(msg))
 
     # PRIVATE METHODS #########################################################
 
     def _open_midi_in(self, port_name):
-        ports = rtmidi.MidiIn().get_ports()
         try:
-            port = next(i for i, port in enumerate(ports)
-                        if port == port_name)
-            return rtmidi.MidiIn().open_port(port)
+            return mido.open_input(port_name)
         except StopIteration:
             messagebox.showerror("Error", "MIDI port not found")
