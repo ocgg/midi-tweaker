8833e15e9e813d414661d5d624a3f6ee58a5a631
Olivier G <zotri@hotmail.com>
2024-04-05 21:20:47 +0200

    rule apply_to rewrite

--- src/models/rule.py
+++ src/models/rule.py
@@ -1,24 +1,25 @@
 class Rule:
-    def __init__(self, attributes, from_msg, to_msg):
-        self.attributes = attributes
-        self.from_msg = from_msg
-        self.to_msg = to_msg
+    def __init__(self, in_msg, out_msg):
+        self.in_msg = in_msg
+        self.out_msg = out_msg
+        self.attributes = list(in_msg.keys())
 
     def apply_to(self, msg):
-        return all([getattr(msg, attribute) == self.from_msg[attribute]
-                    for attribute in self.attributes])
+        # Check if self.in_msg values match with msg
+        for key in self.attributes:
+            if getattr(msg, key) in self.in_msg[key]:
+                continue
+            else:
+                return False
+        return True
 
     def translate(self, msg):
         print('in:', msg)
         print('rule attr:', self.attributes)
-        for attribute in self.attributes:
-            # TODO: manage type conversion
-            if attribute == 'type':
-                continue
-            setattr(msg, attribute, self.to_msg[attribute])
-            print(msg)
-        print('out:', msg)
+        for attribute in self.out_msg.keys():
+            out_value = self.out_msg[attribute][0]
+            # if msg[attribute] == out_value:
+            #     continue
+            setattr(msg, attribute, out_value)
+        print('OUT:', msg)
         return msg
-
-    # def convert_type(self, msg, target_type):
-    #     converted_msg = mido.Message(target_type)
