ee0d1c3db5f626f3ecf032dd3a217f59f832b584
Olivier G <zotri@hotmail.com>
2024-04-08 14:14:33 +0200

    rule form front wip

--- src/views/tabs/rule_form_view.py
+++ src/views/tabs/rule_form_view.py
@@ -14,34 +14,93 @@ class RuleFormView(ttk.Frame):
         super().__init__(master)
         self.TAB = master
 
-        self.in_msg_frame = ttk.Frame(self)
-        self.out_msg_frame = ttk.Frame(self)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        ttk.Label(self, text='Route:').pack(side='top')
-        self.in_msg_form = self._build_form(self.in_msg_frame)
-        self.in_msg_frame.pack(side='top')
+        # IN FORM #############################################################
 
-        ttk.Label(self, text='To:').pack(side='top')
-        self.out_msg_form = self._build_form(self.out_msg_frame)
-        self.out_msg_frame.pack(side='top')
+        in_form_frame = self._build_form(self)
 
-        self.submit_btn = ttk.Button(self, text='OK')
-        self.submit_btn.pack(side='bottom')
+        # SEPARATOR ###########################################################
+
+        separator = ttk.Frame(self)
+        separator.rowconfigure(0, weight=1)
+        separator.rowconfigure(2, weight=1)
+
+        ttk.Separator(separator, orient='vertical').grid(row=0, column=0, sticky='ns')
+        ttk.Label(separator, text='⮕').grid(row=1, column=0)
+        ttk.Separator(separator, orient='vertical').grid(row=2, column=0, sticky='ns')
+
+        # OUT FORM ############################################################
+
+        out_form_frame = self._build_form(self, source='out')
+
+        # SUBMIT BUTTON #######################################################
+
+        self.submit_btn = ttk.Button(self, text='Done', style='big.TButton')
+        self.submit_btn.grid(row=1, column=0, columnspan=3, pady=10)
+
+        # LAYOUT ##############################################################
+
+        in_form_frame.grid(row=0, column=0, sticky='nsew')
+        separator.grid(row=0, column=1, sticky='ns')
+        out_form_frame.grid(row=0, column=2, sticky='nsew')
+
+        # in_msg_form = self._build_form(in_msg_frame)
+        # in_msg_frame.pack(side='top')
+        #
+        # ttk.Label(self, text='To:').pack(side='top')
+        # out_msg_form = self._build_form(out_msg_frame)
+        # out_msg_frame.pack(side='top')
+        #
 
     # LAYOUT ##################################################################
 
-    def _build_form(self, frame):
-        inputs = self._base_inputs(frame)
+    def _build_form(self, frame, source='in'):
+        form_frame = ttk.Frame(self)
+        # form_frame.rowconfigure(0, weight=1)
+        # form_frame.columnconfigure(0, weight=1, uniform='col')
+        # form_frame.columnconfigure(2, weight=1, uniform='col')
 
-        inputs['ch']['label'].grid(row=0, column=0)
-        inputs['ch']['input'].grid(row=0, column=1)
+        title = 'When MIDI IN is :' if source == 'in' else 'Route it to :'
+        form_title = ttk.Label(form_frame, text=title)
+        ch_frame = ttk.Frame(form_frame)
+        type_frame = ttk.Frame(form_frame)
+        val_1_frame = ttk.Frame(form_frame)
+        val_2_frame = ttk.Frame(form_frame)
 
-        inputs['type']['label'].grid(row=0, column=2)
-        msg_type = inputs['type']['input']
-        msg_type.grid(row=0, column=3)
-        msg_type.bind(
-            '<<ComboboxSelected>>',
-            lambda event: self._on_type_selected(event, frame, msg_type))
+        # Channel input ################
+        ch_inputs = self._ch_inputs(ch_frame)
+        ch_inputs['label'].grid(row=0, column=0)
+        ch_inputs['input'].grid(row=0, column=1)
+
+        # Type input ###################
+        type_inputs = self._type_inputs(type_frame)
+        type_inputs['label'].grid(row=0, column=0)
+        type_inputs['input'].grid(row=0, column=1)
+
+        # Layout #######################
+        form_title.grid(row=0, column=0)
+        ch_frame.grid(row=1, column=0)
+        type_frame.grid(row=2, column=0)
+        val_1_frame.grid(row=3, column=0)
+        val_2_frame.grid(row=4, column=0)
+
+        return form_frame
+
+    # def _build_form(self, frame):
+    #     inputs = self._base_inputs(frame)
+    #
+    #     inputs['ch']['label'].grid(row=0, column=0)
+    #     inputs['ch']['input'].grid(row=0, column=1)
+    #
+    #     inputs['type']['label'].grid(row=0, column=2)
+    #     msg_type = inputs['type']['input']
+    #     msg_type.grid(row=0, column=3)
+    #     msg_type.bind(
+    #         '<<ComboboxSelected>>',
+    #         lambda event: self._on_type_selected(event, frame, msg_type))
 
     # INPUT BUILDING ##########################################################
 
@@ -152,26 +211,48 @@ class RuleFormView(ttk.Frame):
         inputs['velocity']['input'].current(0)
         return inputs
 
-    def _base_inputs(self, frame):
-        inputs = {
-            'ch': {
-                'label': ttk.Label(frame, text='CH:'),
-                'input': ttk.Combobox(frame,
-                                      values=self.RANGE_16,
-                                      state='readonly',
-                                      width=4),
-            },
-            'type': {
-                'label': ttk.Label(frame, text='TYPE:'),
-                'input': ttk.Combobox(frame,
-                                      values=self.TYPES,
-                                      state='readonly',
-                                      width=17)
-            },
-        }
-        inputs['ch']['label'].data = {'mido_name': 'channel'}
-        inputs['type']['label'].data = {'mido_name': 'type'}
-        # Default value to 'all'
-        inputs['ch']['input'].current(0)
-        inputs['type']['input'].current(0)
-        return inputs
+    # def _base_inputs(self, frame):
+    #     inputs = {
+    #         'ch': {
+    #             'label': ttk.Label(frame, text='CH:'),
+    #             'input': ttk.Combobox(frame,
+    #                                   values=self.RANGE_16,
+    #                                   state='readonly',
+    #                                   width=4),
+    #         },
+    #         'type': {
+    #             'label': ttk.Label(frame, text='TYPE:'),
+    #             'input': ttk.Combobox(frame,
+    #                                   values=self.TYPES,
+    #                                   state='readonly',
+    #                                   width=17)
+    #         },
+    #     }
+    #     inputs['ch']['label'].data = {'mido_name': 'channel'}
+    #     inputs['type']['label'].data = {'mido_name': 'type'}
+    #     # Default value to 'all'
+    #     inputs['ch']['input'].current(0)
+    #     inputs['type']['input'].current(0)
+    #     return inputs
+
+    def _type_inputs(self, frame):
+        elements = {'label': ttk.Label(frame, text='TYPE:'),
+                    'input': ttk.Combobox(frame,
+                                          values=self.TYPES,
+                                          state='readonly',
+                                          width=17),
+                    }
+        elements['label'].data = {'mido_name': 'type'}
+        elements['input'].current(0)
+        return elements
+
+    def _ch_inputs(self, frame):
+        elements = {'label': ttk.Label(frame, text='CH:'),
+                    'input': ttk.Combobox(frame,
+                                          values=self.RANGE_16,
+                                          state='readonly',
+                                          width=4),
+                    }
+        elements['label'].data = {'mido_name': 'channel'}
+        elements['input'].current(0)  # Default value to 'all'
+        return elements
