c7482a359067d161ac24d329df26ab974003bcc0
Olivier G <zotri@hotmail.com>
2024-04-13 21:41:55 +0200

    wip: validation logic

--- src/controllers/tabs/tab_controller.py
+++ src/controllers/tabs/tab_controller.py
@@ -1,4 +1,8 @@
 from src.models.rule import Rule
+from src.modules.constants import (
+    MIDO_TYPE_TO_VALUES,
+    MIDO_ATTR_RANGE
+)
 
 
 class TabController:
@@ -49,7 +53,7 @@ class TabController:
 
     # CALLBACKS ###############################################################
 
-    # MIDI Learn #####################
+    # MIDI Learn ##########################################
     def _on_midi_learn(self, source, learn_btn):
         self.router.learn(source)
         learn_btn.config(command=lambda:
@@ -60,52 +64,95 @@ class TabController:
         learn_btn.config(command=lambda:
                          self._on_midi_learn(source, learn_btn))
 
-    # MIDI bar refresh ports ##########
+    # MIDI bar refresh ports ##############################
     def _on_refresh(self, source):
         midi_ports = self.router.get_midi_ports(source)
         self.view.update_midi_ports(source, midi_ports)
 
-    # Rule form submit ###############
+    # Rule form submit ####################################
     def _on_rule_submit(self):
         # Stop midi_learn if active
         if self.router.learn_is_active:
             self.router.stop_learn('in')
             self.router.stop_learn('out')
 
-        in_form_data = self.view.frames['form'].in_form.get_form_data()
-        out_form_data = self.view.frames['form'].out_form.get_form_data()
+        in_form_inputs = self.view.frames['form'].in_form.inputs
+        out_form_inputs = self.view.frames['form'].out_form.inputs
 
-        # validate in_form_data
+        # validate form data
+        validation = self._validate_form_data(in_form_inputs, out_form_inputs)
+        is_valid = validation[0]
+        in_form_data = validation[1]
+        out_form_data = validation[2]
+
+        if is_valid:
+            rule = Rule(in_form_data, out_form_data)
+            self.router.add_rule(rule)
+            self.view.frames['list'].update_list(self.router.rules)
+            self.view.display_rules_list()
+        else:
+            self.view.frames['form'].in_form.display_errors(in_form_data)
+            self.view.frames['form'].out_form.display_errors(out_form_data)
+
+    def _validate_form_data(self, in_form_inputs, out_form_inputs):
+        is_valid = True
+        in_form_data = self._process_form_data(in_form_inputs)
+        out_form_data = self._process_form_data(out_form_inputs)
+        # Should convert value types (int & list)
+        # Return a tuple (False, in_form_data, out_form_data) if invalid
+        # with error messages in place of input values for the view to display
+        # Or (True, in_form_data, out_form_data) if valid
+
+        # VALIDATIONS #################
+        # These data are in MIDO_ATTR_RANGE constant
+        # - channel between 1 and 16
+        # - pitch between -8192 and 8191
+        # - other values between 0 and 127
         #
+        # - Out form should not be empty
+        # - ...
 
-        self._clear_inputs(in_form_data, out_form_data)
+        for value in in_form_data.values():
+            if not value:
+                is_valid = False
+        for value in out_form_data.values():
+            if not value:
+                is_valid = False
 
-        # TODO: Rule validations
+        return (is_valid, in_form_data, out_form_data)
 
-        rule = Rule(in_form_data, out_form_data)
-        self.router.add_rule(rule)
+    def _process_form_data(self, form_inputs):
+        form_data = {}
+        form_data['channel'] = form_inputs['channel'].get().replace(' ', '')
+        form_data['type'] = form_inputs['type'].get().replace(' ', '')
 
-        self.view.frames['list'].update_list(self.router.rules)
-        self.view.display_rules_list()
+        # TODO: refacto. Only one for loop
 
-    def _clear_inputs(self, in_form_data, out_form_data):
-        # Removes useless keys in both form_data
-        keys_to_remove = {'in': [], 'out': []}
+        # Get type's input (val1, val2) values if type selected
+        values_names = MIDO_TYPE_TO_VALUES.get(form_data['type']) or []
+        for value in values_names:
+            form_data[value] = form_inputs[value].get().replace(' ', '')
+        # Remove empty values
+        form_data = {k: v for k, v in form_data.items() if v}
+        # convert to int in list or to range
+        for key, value in form_data.items():
+            if value.isdigit():
+                form_data[key] = self._check_int_conversion(key, value)
+            else:
+                form_data[key] = self._check_range_conversion(key, value)
 
-        same_type = in_form_data.get('type') == out_form_data.get('type')
-        for key, value in out_form_data.items():
-            same_val = in_form_data.get(key) == value
-            # CONDITIONS ORDER IS IMPORTANT
-            if same_type and key == 'type':
-                keys_to_remove['out'].append(key)
-            elif same_type and same_val:
-                keys_to_remove['in'].append(key)
-                keys_to_remove['out'].append(key)
-            elif not same_type and same_val:
-                # Case when note_on/note_off & same value
-                keys_to_remove['out'].append(key)
+        return form_data
 
-        for key in keys_to_remove['in']:
-            in_form_data.pop(key)
-        for key in keys_to_remove['out']:
-            out_form_data.pop(key)
+    def _check_int_conversion(self, key, value):
+        value = int(value)
+        if value in MIDO_ATTR_RANGE[key]:
+            return value
+        else:
+            return False
+
+    def _check_range_conversion(self, key, value):
+        try:
+            range_vals = list(map(int, value.split('-')))
+            return range(range_vals[0], range_vals[1] + 1)
+        except ValueError:
+            return False
