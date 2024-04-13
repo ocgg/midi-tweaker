b938b6e3ca70415ec936871e3eeb1df49ec7dc5f
Olivier G <zotri@hotmail.com>
2024-04-13 21:40:56 +0200

    added constants

--- src/modules/constants.py
+++ src/modules/constants.py
@@ -14,3 +14,24 @@ MIDO_TYPE_TO_VAL2 = {
     'note_off': 'velocity',
     'control_change': 'value',
 }
+
+MIDO_TYPE_TO_VALUES = {
+    'note_on': ['note', 'velocity'],
+    'note_off': ['note', 'velocity'],
+    'control_change': ['control', 'value'],
+    'pitchwheel': ['pitch'],
+    'program_change': ['program'],
+    'sysex': ['data'],
+}
+
+MIDO_ATTR_RANGE = {
+    'type': ['note_on', 'note_off', 'control_change', 'pitchwheel',
+             'program_change'],
+    'channel': range(1, 17),
+    'note': range(128),
+    'control': range(128),
+    'velocity': range(128),
+    'value': range(128),
+    'pitch': range(-8192, 8192),
+    'program': range(128),
+}
