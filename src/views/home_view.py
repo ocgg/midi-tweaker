a46d8f110530272f102f4b167b9ec750e45051c9
Olivier G <zotri@hotmail.com>
2024-04-13 15:44:16 +0200

    update text

--- src/views/home_view.py
+++ src/views/home_view.py
@@ -5,5 +5,9 @@ class HomeView(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.label = ttk.Label(self, text="Open a MIDI port to start...")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self.label = ttk.Label(self, text="Open a new tab to start...",
+                               anchor='center')
         self.label.pack(fill='both', expand=True)
