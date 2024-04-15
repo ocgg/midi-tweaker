23089e72643e43ce6de43cb48e7b540d3d1d2df4
Olivier G <zotri@hotmail.com>
2024-04-15 13:22:20 +0200

    test file

--- test-keystation.py
+++ test-keystation.py
@@ -2,6 +2,7 @@ from src.models.application_model import ApplicationModel
 from src.views.application_view import ApplicationView
 from src.controllers.application_controller import ApplicationController
 
+from src.models.rule import Rule
 import mido
 
 
@@ -11,27 +12,32 @@ def main():
     controller = ApplicationController(model, view)
 
     controller.menu_controller._open_tab()
-    test_tab = controller.tab_controller.tabs['New tab']
+    test_tab = controller.tabs_container_controller.tabs['New tab']
 
-    port = mido.get_input_names()[0]
-    test_tab['view'].midi_bars['in']['ports']['combobox'].set(port)
-    test_tab['router'].set_midi_port(None, 'in', port)
+    in_port = mido.get_input_names()[1]
+    out_port = mido.get_output_names()[1]
 
-    rule1_in = {'type': 'note_on', 'velocity': 0}
-    rule1_out = {'type': 'note_off'}
-    test_tab['router'].add_rule(rule1_in, rule1_out)
+    test_tab['view'].midi_bars['in']['ports']['combobox'].set(in_port)
+    test_tab['view'].midi_bars['out']['ports']['combobox'].set(out_port)
+    test_tab['router'].set_midi_port(None, 'in', in_port)
+    test_tab['router'].set_midi_port(None, 'out', out_port)
 
-    rule2_in = {'type': 'control_change', 'control': 1}
-    rule2_out = {'control': 10}
-    test_tab['router'].add_rule(rule2_in, rule2_out)
+    # rule1_in = {'type': 'note_on', 'velocity': 0}
+    # rule1_out = {'type': 'note_off'}
+    # rule1 = Rule(rule1_in, rule1_out)
+    # test_tab['router'].add_rule(rule1)
 
-    rule3_in = {}
-    rule3_out = {'channel': 2}
-    test_tab['router'].add_rule(rule3_in, rule3_out)
-
-    rule4_in = {'channel': 1, 'type': 'control_change', 'control': 10, 'value': 127}
-    rule4_out = {'channel': 2, 'type': 'note_on', 'note': 60, 'velocity': 127}
-    test_tab['router'].add_rule(rule4_in, rule4_out)
+    # rule2_in = {'type': 'control_change', 'control': 1}
+    # rule2_out = {'control': 10}
+    # test_tab['router'].add_rule(rule2_in, rule2_out)
+    #
+    # rule3_in = {}
+    # rule3_out = {'channel': 2}
+    # test_tab['router'].add_rule(rule3_in, rule3_out)
+    #
+    # rule4_in = {'channel': 1, 'type': 'control_change', 'control': 10, 'value': 127}
+    # rule4_out = {'channel': 2, 'type': 'note_on', 'note': 60, 'velocity': 127}
+    # test_tab['router'].add_rule(rule4_in, rule4_out)
 
     test_tab['view'].frames['list'].update_list(test_tab['router'].rules)
 
